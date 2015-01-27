d49f990d096a224491ba203d05e8f811a13498a4
Dag Olav <dagolav@arkitektum.no>
2015-01-27 16:20:08 +0100

    New endpointurl

--- Kartverket.MetadataMonitor/Models/Constants.cs
+++ Kartverket.MetadataMonitor/Models/Constants.cs
@@ -2,7 +2,7 @@
 {
     public class Constants
     {
-        public const string EndpointUrlInspire = "http://inspire-geoportal.ec.europa.eu/GeoportalProxyWebServices/resources/INSPIREResourceTester?dontGenerateLayerPreviews=anyvalue";
+        public const string EndpointUrlInspire = "http://159.162.102.159/INSPIREWebServices/resources/INSPIREResourceTester";
         public const string EndpointUrlGeoNorgeCsw = "http://www.geonorge.no/geonetwork/srv/nor/csw";
     }
 }
