df280febb10b4562a119098402b558457c2dec2d
Dag Olav <dagolav@arkitektum.no>
2015-01-28 10:27:57 +0100

    Changed to internal ip endpoint

--- Kartverket.MetadataMonitor/Models/Constants.cs
+++ Kartverket.MetadataMonitor/Models/Constants.cs
@@ -2,7 +2,7 @@
 {
     public class Constants
     {
-        public const string EndpointUrlInspire = "http://159.162.102.159/INSPIREWebServices/resources/INSPIREResourceTester";
+        public const string EndpointUrlInspire = "http://10.2.10.58/INSPIREWebServices/resources/INSPIREResourceTester";
         public const string EndpointUrlGeoNorgeCsw = "http://www.geonorge.no/geonetwork/srv/nor/csw";
     }
 }
