eeb94ed875d5104482c2067dfb1c1f68a5ebb72f
Benjamin Dehli <benjamin@arkitektum.no>
2016-06-08 13:25:12 +0200

    Add and include Bower components

--- Kartverket.MetadataMonitor/App_Start/BundleConfig.cs
+++ Kartverket.MetadataMonitor/App_Start/BundleConfig.cs
@@ -8,11 +8,15 @@ namespace Kartverket.MetadataMonitor
         // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
         public static void RegisterBundles(BundleCollection bundles)
         {
-            //bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-            //            "~/Scripts/jquery-{version}.js"));
+            bundles.Add(new StyleBundle("~/Content/bower_components/kartverket-felleskomponenter/assets/css/styles").Include(
+                "~/Content/bower_components/kartverket-felleskomponenter/assets/css/vendor.min.css",
+                "~/Content/bower_components/kartverket-felleskomponenter/assets/css/main.min.css"
+            ));
 
-            //bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
-            //            "~/Scripts/jquery-ui-{version}.js"));
+            bundles.Add(new ScriptBundle("~/Content/bower_components/kartverket-felleskomponenter/assets/scripts").Include(
+               "~/Content/bower_components/kartverket-felleskomponenter/assets/js/vendor.min.js",
+               "~/Content/bower_components/kartverket-felleskomponenter/assets/js/main.js"
+           ));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.unobtrusive*",
@@ -22,41 +26,6 @@ namespace Kartverket.MetadataMonitor
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
-
-            //bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
-            //            "~/Content/themes/base/jquery.ui.core.css",
-            //            "~/Content/themes/base/jquery.ui.resizable.css",
-            //            "~/Content/themes/base/jquery.ui.selectable.css",
-            //            "~/Content/themes/base/jquery.ui.accordion.css",
-            //            "~/Content/themes/base/jquery.ui.autocomplete.css",
-            //            "~/Content/themes/base/jquery.ui.button.css",
-            //            "~/Content/themes/base/jquery.ui.dialog.css",
-            //            "~/Content/themes/base/jquery.ui.slider.css",
-            //            "~/Content/themes/base/jquery.ui.tabs.css",
-            //            "~/Content/themes/base/jquery.ui.datepicker.css",
-            //            "~/Content/themes/base/jquery.ui.progressbar.css",
-            //            "~/Content/themes/base/jquery.ui.theme.css"));
-
-            /* setup bundle for twitter bootstrap 
-            bundles.Add(new StyleBundle("~/Content/bootstrap/css/bundle").Include(
-                "~/Content/bootstrap/css/bootstrap.css",
-                "~/Content/bootstrap/css/bootstrap-responsive.css"));
-             
-            bundles.Add(new ScriptBundle("~/Content/bootstrap/js/bundle").Include("~/Content/bootstrap/js/bootstrap.js"));
-            * */
-
-            //bundles.Add(new ScriptBundle("~/Content/bootstrap/js/bundle").Include("~/Scripts/bootstrap.js"));
-
-            //bundles.Add(new StyleBundle("~/Content/css")
-            //    .Include("~/Content/bootstrap.css")
-            //    .Include("~/Content/chosen.css")
-            //    .Include("~/Content/common.css")
-            //    .Include("~/Content/navbar.css")
-            //    .Include("~/Content/searchbar.css")
-            //    .Include("~/Content/site.css")
-            //    );
-                
-
         }
     }
 }
